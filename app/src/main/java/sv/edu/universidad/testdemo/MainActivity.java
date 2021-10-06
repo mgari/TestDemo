41c4ac7110095376874874689954383d7adee86c
hmvanegas <henry.vanegas@itca.edu.sv>
2021-10-05 22:18:11 -0600

    Segundo cambio

--- app/src/main/java/sv/edu/universidad/testdemo/MainActivity.java
+++ app/src/main/java/sv/edu/universidad/testdemo/MainActivity.java
@@ -3,6 +3,8 @@ package sv.edu.universidad.testdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Prueba de cambios en Github",Toast.LENGTH_LONG).show();
+
     }
 }
